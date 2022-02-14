670995690e0df1db85f422dbb2a6eb21224384b8
PedramPourhakim <shemuel1226@gmail.com>
2022-02-14 22:13:15 +0330

    قسمت مربوط به صفحه ی اصلی داینامیک گردید و ویو کامپوننت ها اضافه شد مرحله ی بعدی قسمت صفحه ی ارتباط با ما میباشد

---
+++ Solution1/WebApplication1/Models/KhasTarin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class KhasTarin
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Client { get; set; }
+
+        public KhasTarin(long id, string title, string description, string image, string client)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Image = image;
+            Client = client;
+        }
+    }
+}
---
+++ Solution1/WebApplication1/ViewComponents/PedramViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.ViewComponents
+{
+    public class PedramViewComponent :ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var Articles = new List<Article>
+            {
+                new Article(1,"آموزش پست گذاشتن","بهترین آموزش پست گذاشتن در اینستاگرام.","blog-post-thumb-1.jpg"),
+                new Article(2,"برنامه نویسی","از چه زبان برنامه نویسی استفاده کنیم مفید تر است؟","blog-post-thumb-2.jpg"),
+                new Article(3,"آموزش آبیاری گیاهان دریایی"," آبیاری گیاهان دریاایی .بهترین آموزش روشهای .","blog-post-thumb-3.jpg"),
+                new Article(4,"آموزش رانندگی با کیبورد","بهترین آموزش رانندگی با کیبورد.","blog-post-thumb-4.jpg"),
+            };
+            return View("Second", Articles);
+        }
+    }
+}
